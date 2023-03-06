7a6d149ad0b865e76df197a0014df700a836b428
Luke Calderon <luke.a.calderon@gmail.com>
2023-03-06 10:51:45 -0600

    select and update exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,9 +1,28 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
 
-SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
 
-SELECT genre FROM albums WHERE name = 'Nevermind';
+# SELECT  'The genre for nevermind' AS '====EXERCISE 3';
 
-SELECT * FROM albums WHERE release_date = '1990';
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name
+FROM albums
+WHERE release_date between 1990 AND 1999;
+
+SELECT name
+FROM albums
+WHERE sales < 20;
+
+SELECT name FROM albums
+WHERE genre LIKE '%rock%';
+
+
