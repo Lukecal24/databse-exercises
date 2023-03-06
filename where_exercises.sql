f7b64b08bc7db7664b7b08670ba772a235a60269
Luke Calderon <luke.a.calderon@gmail.com>
2023-03-06 13:53:39 -0600

    where exercises

---
+++ where_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
