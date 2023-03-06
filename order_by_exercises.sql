2fd62f0a5b1a1c0fdbf148e71faa590885e8101d
Luke Calderon <luke.a.calderon@gmail.com>
2023-03-06 14:20:03 -0600

    finished limit exercises also

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,10 +2,15 @@ USE employees;
 
 SELECT *
 FROM employees
-WHERE (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-    AND gender = 'M';
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ASC, first_name ASC;
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%E%'
+ORDER BY emp_no DESC;
+
 
 
 SELECT *
